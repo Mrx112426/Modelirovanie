1675821c8816440716cee3a0e8c747562046c3a3
Mrx112426 <victor.dunduk@mail.ru>
2025-03-24 22:23:39 +0300

    Исправлен подсчет значений в первом интервале

--- Lab4/multicod.py
+++ Lab4/multicod.py
@@ -9,21 +9,33 @@ print("Введите 1 если хотите проверить вариант 
 p = int(input())
 if p == 1:
     df = pd.read_csv('Варианты_практика_1.csv', sep=';')
-    df = df.applymap(lambda x: round(float(str(x).replace(',', '.')), 2))# для проверки ручной части, когда числа в формате xx,xxx
+    df = df.map(lambda x: round(float(str(x).replace(',', '.')), 2))  # Исправлено applymap на map
     variant_column = df[f'{(k - 1) % 10 + 1}']
 else:
     df = pd.read_csv('myData.csv')
     variant_column = df[f'variant_{(k - 1) % 20 + 1}']
 
+def z(data):
+    return int(np.sqrt(len(data)))
+
+def razmax(data):
+    return np.max(data) - np.min(data)
+
+def lengt_interval(data):
+    return razmax(data) / z(data)
+
 def sample_mean(data):
     return sum(data) / len(data)
+
 
 def sample_variance(data):
     mean = sample_mean(data)
     return sum((x - mean) ** 2 for x in data) / (len(data) - 1)
 
+
 def std_deviation(data):
     return np.sqrt(sample_variance(data))
+
 
 def mode(data):
     mode_result = stats.mode(data)
@@ -31,7 +43,6 @@ def mode(data):
         return mode_result.mode[0]
     else:
         return mode_result.mode
-
 
 def excess_kurtosis(data):
     n = len(data)
@@ -43,41 +54,52 @@ def excess_kurtosis(data):
 def median(data):
     return np.median(data)
 
+
 def skewness(data):
     return stats.skew(data)
 
+
 def range_and_intervals(data):
-    data_range = max(data) - min(data)
+    data_min = min(data)
+    data_max = max(data)
+    data_range = data_max - data_min
     num_intervals = int(np.sqrt(len(data)))
-    return data_range, num_intervals
+
+    bin_width = data_range / num_intervals
+    bins = [data_min + i * bin_width for i in range(num_intervals)]
+    bins.append(data_max + 0.0001)  # Небольшая поправка для включения максимума
+    return bins
+
 
 def plot_histogram(data):
-    count, bins, ignored = plt.hist(data, bins='auto', alpha=0.7, color='blue', edgecolor='black')
+    bins = range_and_intervals(data)
+    count, bins, patches = plt.hist(data, bins=bins, alpha=0.7, color='blue', edgecolor='black')
 
-    plt.xticks(bins)
 
-    # Подписи и заголовок
+    if p == 1:
+        plt.xticks(bins)
+        plt.yticks(count)
+
     plt.title('Гистограмма')
     plt.xlabel('Границы интервалов')
     plt.ylabel('Частота')
-
     plt.show()
+    return count, bins
+
 
 def plot_frequency_polygon(data):
-    count, bins = np.histogram(data, bins='auto')
-
+    count, bins = plot_histogram(data)
     bin_centers = (bins[:-1] + bins[1:]) / 2
 
-    # Строим полигон частот
     plt.plot(bin_centers, count, marker='o', color='red', linestyle='-', linewidth=2)
-
-    plt.xticks(bin_centers)
-    plt.yticks(count)
-
+    if p == 1:
+        plt.xticks(bin_centers)
+        plt.yticks(count)
     plt.title('Полигон частот')
     plt.xlabel('Значения')
     plt.ylabel('Частота')
     plt.show()
+
 
 def empirical_distribution(data):
     data_sorted = np.sort(data)
@@ -88,9 +110,12 @@ def empirical_distribution(data):
     plt.ylabel('Проба')
     plt.show()
 
+
 data = variant_column.values
 
-# Вычисления
+print("Количество интервалов:", z(data))
+print("Размах", razmax(data))
+print("Длина интервала",lengt_interval(data))
 print("Выборочное среднее:", sample_mean(data))
 print("Выборочная дисперсия:", sample_variance(data))
 print("Среднеквадратическое отклонение:", std_deviation(data))
@@ -99,11 +124,5 @@ print("Медиана:", median(data))
 print("Коэффициент асимметрии:", skewness(data))
 print("Коэффициент эксцесса (без коррекции):", excess_kurtosis(data))
 
-data_range, num_intervals = range_and_intervals(data)
-print(f"Размах выборки: {data_range}")
-print(f"Количество интервалов: {num_intervals}")
-
-
-plot_histogram(data)
 plot_frequency_polygon(data)
 empirical_distribution(data)
